2e756beae72a49bf51e8d7c95235620d88010468
rachidassouani <rachidassouani@gmail.com>
2024-06-04 15:57:16 +0100

    Solved user error during the startup of the project

---
+++ book-social-network-api/src/main/java/io/rachidassouani/booksocialnetworkapi/security/SecurityFilterChainConfig.java
@@ -0,0 +1,46 @@
+package io.rachidassouani.booksocialnetworkapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityFilterChainConfig {
+
+    private AuthenticationProvider authenticationProvider;
+    private JwtFilter jwtFilter;
+
+   public SecurityFilterChainConfig(AuthenticationProvider authenticationProvider,
+                         JwtFilter jwtFilter) {
+        this.authenticationProvider = authenticationProvider;
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(
+                                "/auth/**",
+                                "/webjars/**"
+                ).permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
